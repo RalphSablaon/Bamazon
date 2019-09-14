d027a1eadeac4f127ef8a25cbec3cdc919fa2201
Ralph Sablaon <rsablaon88@gmail.com>
2019-09-13 18:04:45 -0700

    coded database and table with data

---
+++ bamazon.sql
@@ -0,0 +1,43 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+	item_id INTEGER(4) NOT NULL,
+    PRIMARY KEY (item_id),
+    product_name VARCHAR(100) NOT NULL,
+    department VARCHAR(100) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INTEGER(10) NOT NULL
+);
+
+INSERT INTO products (item_id, product_name, department, price, stock_quantity)
+VALUES (9, "Phone Charger USB Cable", "Electronics". 9.99, 50);
+
+INSERT INTO products (item_id, product_name, department, price, stock_quantity)
+VALUES (13, "Teddy Bear", "Toys". 14.99, 300);
+
+INSERT INTO products (item_id, product_name, department, price, stock_quantity)
+VALUES (76, "Wireless Controller", "Video Games". 59.99, 2000);
+
+INSERT INTO products (item_id, product_name, department, price, stock_quantity)
+VALUES (145, "Wireless Bluetooth Headphones", "Electronics". 129.99, 5000);
+
+INSERT INTO products (item_id, product_name, department, price, stock_quantity)
+VALUES (285, "Playstation 4 w/ 3 Games Bundle", "Video Games". 499.99, 400);
+
+INSERT INTO products (item_id, product_name, department, price, stock_quantity)
+VALUES (583, "Fidget Spinner", "Toys". 2.99, 50000);
+
+INSERT INTO products (item_id, product_name, department, price, stock_quantity)
+VALUES (872, "Call of Duty Console Game", "Video Games". 9.99, 20000);
+
+INSERT INTO products (item_id, product_name, department, price, stock_quantity)
+VALUES (1004, "Pioneer XDJ-RR DJ System", "Electronics". 999.99, 250);
+
+INSERT INTO products (item_id, product_name, department, price, stock_quantity)
+VALUES (1477, "Off-Road R/C Car", "Toys". 39.99, 378);
+
+INSERT INTO products (item_id, product_name, department, price, stock_quantity)
+VALUES (5395, "Easy Bake Oven Set", "Toys". 24.99, 50);
